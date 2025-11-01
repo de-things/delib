dfcd3afa3ae981e0f0a6a971cbe1146472e1256e
Mikhail Supov <supchyan@yandex.ru>
2025-11-01 21:59:11 +0300

    !!!

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# delib
+> [!CAUTION]
+> Docs is WIP.
+
+### Summary
+Core network lib to communicate with server side of de:things devices.
+
+### Install
+Clone repo into local arduino librares folder. Lib file should be located like:
+```
+C:\Users\your_username\Documents\Arduino\libraries\delib\delib.h
+```
+
+### Usage
+```cpp
+// firmware.ino
+
+// include delib
+#include <delib.h>
+
+// define mac
+uint8_t mac[] = { 13, 13, 13, 13, 13, 13 };
+
+// define delib object
+delib Delib;
+
+void setup() {
+  // set wifi credentials (ssid, key)
+  Delib.set_wifi_credentials("whale_wifi", "wha1e_p@ss");
+
+  // initialize delib server
+  Delib.init(mac);
+}
+
+void loop() {
+  // handle client<>server communication
+  Delib.update();
+}
+
+```
---
+++ delib.h
@@ -0,0 +1,152 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <Ethernet.h>
+
+class delib {
+public:
+    /**
+    * WLAN server used to handle wlan requests whenever WLAN is in use.
+    */
+    ESP8266WebServer wlan_server = ESP8266WebServer(80);
+    /**
+    * Ethernet server used to handle ethernet requests whenever Ethernet is in use.
+    */
+    EthernetServer ethn_server = EthernetServer(80);
+
+    /**
+    * Initialization method. Use it to start a proper server after pre-requirements have finished.
+    */
+    void init(uint8_t mac[6]) {
+        Serial.begin(74880);
+        while (!Serial) {}
+
+        Serial.println();
+        delay(2000);
+
+        if (mac == bad_mac) {
+            Serial.println("Cannot use MAC address specified. Initialization failed.");
+            return;
+        }
+
+        Serial.println("Selected Ethernet profile.");
+
+        delay(500);
+
+        Ethernet.setMACAddress(mac);
+
+        if (Ethernet.begin(mac) == 0) { // if establishing ethernet returned `0` (error), try to use wlan instead
+            Serial.println("Cannot access network through ethernet. Is cable plugged in?");
+            Serial.println();
+            Serial.println("Selected WLAN profile."); 
+
+            delay(500);
+
+            if (wifi_ssid == "") { // return if no ssid specified.
+                Serial.println("SSID is empty. Have you tried to use set_wifi_credentials(ssid, key) before init(mac) to define wifi credentials?"); 
+                return;
+            }
+
+            WiFi.mode(WIFI_STA);
+            wifi_set_macaddr(STATION_IF, mac);
+            WiFi.begin(wifi_ssid, wifi_key); // begin the connection to specified wlan
+
+            Serial.print("Connecting to "); Serial.println(wifi_ssid);
+
+            while (WiFi.status() != WL_CONNECTED)
+            {
+                delay(500);
+                Serial.print(".");
+            }
+            Serial.println();
+
+            Serial.print("Connected, IP address: "); Serial.println(WiFi.localIP());
+
+            // start wlan server
+            wlan_server.begin();
+        }
+        else { // ethernet connection established, so start `ethn_server`
+            Serial.print("Connected, IP address: "); Serial.println(Ethernet.localIP());
+            // start ethernet server
+            ethn_server.begin();
+        }
+    }
+    /**
+    * Main loop of `delib`. Handles client<>server communication if established.
+    */
+    void update() {
+        if (state == 0) { // ethernet client handling
+            EthernetClient client = ethn_server.available();
+            if (client) {
+                while (client.connected()) {
+                    if (client.available()) {
+                        // char c = client.read();
+                        // ethn_server.write(client.read());
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-Type: text/plain");
+                        client.println("Connection: close");
+                        client.println();
+                        client.println("response message");
+                    }
+                }
+                delay(1);
+                client.stop();
+            }
+        }
+        if (state == 1) { // wlan client handling
+            wlan_server.handleClient();
+        }
+    }
+    /**
+    * Returns true if client's wlan request is valid (POST and has a proper SECRET). 
+    * Use it in wlan client handlers to decline unauthorized requests.
+    * You can define device secret with a `set_secret(your_secret);`
+    */
+    bool auth_wlan_request() {
+        if (wlan_server.method() == HTTP_POST) {
+            Serial.println("Received POST");
+        }
+        else {
+            Serial.println("Wrong method");
+            wlan_server.send(405, "text/plain", "Wrong method");
+            return false;
+        }
+
+        if (wlan_server.arg("plain") == secret) {
+            wlan_server.send(200, "text/plain", "Fine secret");
+        }
+        else {
+            Serial.println("Bad secret");
+            wlan_server.send(403, "text/plain", "Bad secret");
+            return false;
+        }
+        return true;
+    }
+    /**
+    * Sets a device wlan credentials. If not set, delib will use ethernet instead.
+    */
+    void set_wifi_credentials(String ssid, String key) {
+        wifi_ssid = ssid;
+        wifi_key  = key;
+    }
+    /**
+    * Sets a device secret to decline requests with a different one. (WHALE by default)
+    */
+    void set_secret(String new_secret) {
+        secret = new_secret;
+    }
+private:
+    // secret to validate wlan requests
+    String secret = "WHALE";
+
+    // -1: default;
+    //  0: ethernet;
+    //  1: wlan;
+    int state = -1;
+
+    uint8_t bad_ip[4];
+    uint8_t bad_mac[6];
+
+    // ssid, key
+    String wifi_ssid = "";
+    String wifi_key  = "";
+};
